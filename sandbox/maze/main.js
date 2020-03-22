c47e8cd40eb0f51976e5665c92751dd248cec767
serhii.zamoshnikov <sergey.zamoshnikov@dev-pro.net>
2020-03-22 14:32:34 +0200

    Adds first request task

--- sandbox/maze/main.js
+++ sandbox/maze/main.js
@@ -1,77 +1,81 @@
 import { Gamefield } from '../libs/gamefield.js';
-import { setCellClass } from '../snake/gamefield.js';
 
 /** @type Gamefield */
 let gamefield;
 
-const directions = {
-  north: { x: 0, y: -1 },
-  south: { x: 0, y: 1 },
-  west: { x: -1, y: 0 },
-  east: { x: 1, y: 0 },
-}
+/** @type string[] */
+let crackedWalls = [];
+
+const directions = [
+  { name: 'north', x: 0, y: -1 },
+  { name: 'south', x: 0, y: 1 },
+  { name: 'west', x: -1, y: 0 },
+  { name: 'east', x: 1, y: 0 },
+];
 
 function main() {
   gamefield = new Gamefield(document.getElementById('gamefield'));
-  gamefield.init({ width: 51, height: 31 });
+  gamefield.init({ width: 61, height: 41, cellSize: 15 });
 
   setupWalls(gamefield);
-  const startX = Math.floor(Math.random() * ((gamefield.width - 1) / 2)) * 2 + 1;
-  const startY = Math.floor(Math.random() * ((gamefield.height - 1) / 2)) * 2 + 1;
-  dissolve(startX, startY);
+  const startX = Math.floor(Math.random() * (Math.floor(gamefield.width / 2))) * 2 + 1;
+  const startY = Math.floor(Math.random() * (Math.floor(gamefield.height / 2))) * 2 + 1;
+  createMaze(startX, startY);
 }
 
-function dissolve(initX, initY) {
-  /** @type string[] */
-  const borderWalls = [];
-  /** @type string[] */
-  const checkedSpace = [];
-
-  function findWalls(currX, currY) {
-    const cellId = `x${currX}y${currY}`
-    if (checkedSpace.includes(cellId)) return;
-
-    switch (gamefield.getCellClass(currX, currY)) {
-      case 'permawall':
-        return;
-      case 'wall':
-        if (borderWalls.includes(cellId)) {
-          borderWalls.splice(borderWalls.indexOf(cellId), 1);
-        } else {
-          borderWalls.push(cellId);
-        }
-        return;
-      default:
-        checkedSpace.push(cellId);
-        Object.values(directions).forEach(({ x, y }) => {
-          findWalls(currX + x, currY + y);
-        });
-    }
+function exploreCell(currX, currY) {
+  const cellId = `x${currX}y${currY}`;
+  switch (gamefield.getCellClass(currX, currY)) {
+    // A common wall found. We'll put some cracks on it and track its location.
+    case 'wall':
+      crackedWalls.push(cellId);
+      gamefield.setCellClass(currX, currY, 'crackedwall');
+      return;
+    // a wall found second time from other side, so it should not be removed.
+    // Remove its location from crackedWalls array.
+    case 'crackedwall':
+      crackedWalls.splice(crackedWalls.indexOf(cellId), 1);
+      gamefield.setCellClass(currX, currY, 'permawall');
+      return;
+    // an empty space. Need to check out its suroundings.
+    // Remove all classes to prevent any further action for this cell
+    case 'untouched':
+      gamefield.setCellClass(currX, currY, '');
+      directions.forEach(({ x, y }) => exploreCell(currX + x, currY + y));
+      return;
+    default:
   }
+}
 
-  findWalls(initX, initY);
+function dissolveRandomWall() {
+  if (crackedWalls.length) {
+    // pick a random wall from the array. Remove it from array.
+    const [removeWallCellId] = crackedWalls.splice(Math.floor(Math.random() * crackedWalls.length), 1);
+    const [x, y] = removeWallCellId.substring(1).split('y');
+    // mark as empty unprocessed space
+    gamefield.setCellClass(x, y, 'untouched');
+    exploreCell(Number(x), Number(y));
 
-  while (borderWalls.length) {
-    const removeWallIndex = Math.floor(Math.random() * borderWalls.length);
-    const [removeWallCellId] = borderWalls.splice(removeWallIndex, 1);
-    const [cellX, cellY] = removeWallCellId.substring(1).split('y');
-    const cell = gamefield.setCellClass(cellX, cellY, '');
-    findWalls(Number(cellX), Number(cellY));
+    // Schedule an attempt to dissolve one more wall
+    setTimeout(() => dissolveRandomWall(), 10);
   }
+}
+
+function createMaze(initX, initY) {
+  exploreCell(initX, initY);
+  dissolveRandomWall();
 }
 
 function setupWalls(gf) {
-  for (let y = 0; y < gf.height; y++) {
-    for (let x = 0; x < gf.width; x++) {
-      gf.setCellClass(x, y, 'wall');
-    }
-  }
   for (let y = 1; y < gf.height; y = y + 2) {
     for (let x = 1; x < gf.width; x = x + 2) {
-      gf.setCellClass(x, y, '');
+      gf.setCellClass(x, y, 'untouched');
+      gf.setCellClass(x + 1, y, 'wall');
+      gf.setCellClass(x, y + 1, 'wall');
       gf.setCellClass(x + 1, y + 1, 'permawall');
     }
   }
+
   for (let y = 0; y < gf.height; y = y + 1) {
     gf.setCellClass(0, y, 'permawall');
     gf.setCellClass(gf.width - 1, y, 'permawall');
---
+++ sandbox/request/server.js
@@ -0,0 +1,38 @@
+/**
+ * The actual code which is used on the server for this assignment
+ */
+const http = require('http');
+
+const PORT = process.env.PORT || 5000
+let responseCount = 0;
+
+/**
+ * Sets CORS headers to response
+ * @param {http.ServerResponse} res
+ */
+function setCors(res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+}
+
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+const handleRequest = (req, res) => {
+  if (req.url === '/hw/42') {
+    setTimeout(() => {
+      setCors(res);
+      responseCount++;
+      res.write(`Your first response! ${responseCount} responses sent so far.`);
+      res.end();
+    }, Math.random() * 3000);
+    return;
+  }
+
+  res.statusCode = 404;
+  res.end();
+}
+
+http.createServer(handleRequest)
+  .listen(PORT);
